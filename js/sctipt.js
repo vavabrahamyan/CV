a3dfec31b89fbc28b77353441bab490bf78be2c8
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2021-07-08 01:50:32 +0400

    created responsive online cv

---
+++ js/sctipt.js
@@ -0,0 +1,157 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+
+
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    if (getWidth() < 968) {
+        if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+        else scrollTop.classList.remove('show-scroll')
+    }
+
+}
+window.addEventListener('scroll', scrollTop)
+
+// dark theme
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+
+}
+
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+    console.log(selectedTheme)
+
+    resumeLink = selectedTheme === 'dark' ? './pdf/cv-dark-mode.pdf' : './pdf/cv-light-mode.pdf'
+
+    console.log(resumeLink)
+})
+
+
+// change document size to A4 format
+
+function scaleCv() {
+    document.body.classList.add('scale-cv')
+}
+
+// feedback size after downloading
+
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
+
+/*=><= GENERATE PDF =><=*/
+
+// // PDF generated area
+
+let print_element = document.getElementById('cv-area')
+
+let button = document.getElementById('resume-button')
+
+let opt = {
+    margin: 0,
+    filename: 'vavabrahamyan_CV.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
+}
+
+
+let generatePDF = (button, element) => {
+    button.addEventListener('click', () => {
+        scaleCv()
+        html2pdf(element, opt)
+        setTimeout(removeScale, 5000)
+    })
+}
+
+generatePDF(button, print_element)
+
+
+// copy email when click
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+// download PDF in mobile responsive
+
+
+
+
+
+
+
+
+function getWidth() {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+}
